9c772506be4ca28b095c15d18db34936b07b37a4
Nicolas Casajus <rdev.nc@gmail.com>
2024-12-03 15:29:33 +0100

    Revert "update 2024"
    This reverts commit a97f69ad942c5f0dec77749c0a53109ad100fe21.

--- R/functions.R
+++
@@ -1,9 +0,0 @@
-make_hist <- function(dat) {
-  
-  #dat = targets::tar_read("data")
-  
-  ggplot2::ggplot(data = dat, mapping = ggplot2::aes(x = Ozone)) +
-    ggplot2::geom_histogram()
-  
-  
-}
--- make.R
+++
@@ -1,9 +0,0 @@
-
-# ---- project execution
-targets::tar_config_set(
-  store  = "outputs/pipeline/",
-  script = "analyses/pipeline.R"
-)
-
-targets::tar_visnetwork()
-targets::tar_make()
